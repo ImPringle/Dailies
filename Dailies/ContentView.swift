58f85f2181c5ff56d0c79e8e02b33cdd7bea8b03
Mario Pringle <itspringle1213@gmail.com>
2024-08-14 11:47:22 -0700

    ProjectSetup

---
+++ Dailies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dailies
+//
+//  Created by Mario Zuniga on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dailies/DailiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailiesApp.swift
+//  Dailies
+//
+//  Created by Mario Zuniga on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DailiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
